13bd5f38f4ded88c80d2b21c78dbcda75acc19ae
carolineablynch <carolineablynch@gmail.com>
2021-03-16 19:53:51 -0400

    adding rotuer.get functionality

--- develop/routes/index.js
+++ develop/routes/index.js
@@ -1,2 +1,21 @@
 const router = require("express").Router();
 const { Workout } = require("../models");
+
+router.get("/api/workouts", async (req, res) => {
+    try {
+      const workouts = await Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: { $sum: "$exercises.duration" }
+          }
+        }
+      ]).exec();
+  
+      res.json(workouts);
+    } catch(error) {
+      res.sendStatus(500);
+  
+      throw error;
+    }
+  });
+  

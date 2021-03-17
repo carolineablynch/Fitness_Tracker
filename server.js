c7ff9e6b1bb84488e8580008641ad5d5999d024b
carolineablynch <carolineablynch@gmail.com>
2021-03-16 20:35:12 -0400

    finishing server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const routes = require("./routes/index.js");
+const routes = require("./routes/index");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -11,3 +11,9 @@ mongoose.connect("mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
+app.use(express.static("public", { extensions: ["html"] }));
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(routes);
+
+app.listen(PORT, () => console.log(`🚀 Listening on PORT ${PORT}`));
